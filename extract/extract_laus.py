6d2862e345f1563bc72331767b2dab994cc4ab3e
Raymond Kim <hkapray@gmail.com>
2026-08-24 21:49:27 -0500

    add LAUS extraction script, pull successfully into duckdb

---
+++ extract/extract_laus.py
@@ -0,0 +1,72 @@
+import requests
+import pandas as pd
+import duckdb
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+api_key = os.getenv("BLS_API_KEY")
+
+# One entry per metro: unemployment rate, unemployment figure, employment figure, labor force
+series_ids = {
+    "chicago": ["LAUMT171698000000003", "LAUMT171698000000004", "LAUMT171698000000005", "LAUMT171698000000006"],
+    # "austin": [...],
+    # "dc": [...],
+    # "nyc": [...],
+}
+
+def extract_laus(series_ids, start_year="2016", end_year="2026"):
+    url = "https://api.bls.gov/publicAPI/v2/timeseries/data/"
+
+    # all_series = []
+    # for metro_series in series_ids.values():
+    #     for sid in metro_series:
+    #         all_series.append(sid)
+
+    # nested list comprehension
+    all_series = [sid for metro_series in series_ids.values() for sid in metro_series]
+
+    payload = {
+        "seriesid": all_series,
+        "startyear": start_year,
+        "endyear": end_year,
+        "catalog": True,
+        "calculations": True,
+        "annualaverage": True,
+        "aspects": False,
+        "registrationkey": api_key
+    }
+
+    response = requests.post(url, json=payload)
+    response.raise_for_status()
+    data = response.json()
+
+    series_lookup = [] # catalog data for metadata (area name, occupation, etc)
+    records = [] # raw data table
+    for series in data["Results"]["series"]:
+        series_lookup.append({
+            "series_id": series["seriesID"],
+            "area": series["catalog"]["area"],
+            "series_title": series["catalog"]["series_title"]
+            # add more fields later if needed
+        })
+        for point in series["data"]:
+            records.append({
+                "series_id": series["seriesID"],
+                "year": point["year"],
+                "period": point["period"],
+                "period_name": point["periodName"],
+                "value": point["value"],
+            })
+
+    df_lookup = pd.DataFrame(series_lookup).drop_duplicates()
+    df_data = pd.DataFrame(records)
+
+    return df_lookup, df_data
+
+if __name__ == "__main__":
+    df_lookup, df_data = extract_laus(series_ids)
+    con = duckdb.connect("dev.duckdb")
+    con.execute("CREATE OR REPLACE TABLE raw_laus_series AS SELECT * FROM df_lookup")
+    con.execute("CREATE OR REPLACE TABLE raw_laus_data AS SELECT * FROM df_data")
+    print(f"Loaded {len(df_lookup)} series, {len(df_data)} data rows")
