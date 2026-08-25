5883d625de6c1d8212a2f088dc913fbf7e08182b
Raymond Kim <hkapray@gmail.com>
2026-08-25 18:12:55 -0500

    view duckdb data

--- test.py
+++ test.py
@@ -1,6 +1,13 @@
+#%%
+
 from dotenv import load_dotenv
 import os
+import duckdb
 
 load_dotenv()
 api_key = os.getenv("BLS_API_KEY")
 print(api_key)
+
+con = duckdb.connect("dev.duckdb")
+con.execute("SELECT * FROM raw_laus_series").df()
+# %%
