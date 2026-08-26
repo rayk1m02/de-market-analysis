bccc36f7ab3203f2ef08ea3e4d915a56ba64c482
Raymond Kim <hkapray@gmail.com>
2026-08-26 14:14:50 -0500

    extracting oews data from bulk file

---
+++ extract/extract_oews.py
@@ -0,0 +1,19 @@
+'''
+https://download.bls.gov/pub/time.series/oe/
+
+oe.area
+48	0012420	M	Austin-Round Rock-San Marcos, TX
+53	0042660	M	Seattle-Tacoma-Bellevue, WA
+17	0016980	M	Chicago-Naperville-Elgin, IL-IN
+11	0047900	M	Washington-Arlington-Alexandria, DC-VA-MD-WV
+36	0035620	M	New York-Newark-Jersey City, NY-NJ
+
+oe.occupation
+151242	Database Administrators
+151243	Database Architects
+152051	Data Scientists
+151252	Software Developers
+
+oe.series
+notepad++
+'''
