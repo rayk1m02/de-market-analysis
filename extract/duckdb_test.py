8bdb772f0909d1ad3cb56e20bd77585377cf4cda
Raymond Kim <hkapray@gmail.com>
2026-08-29 12:59:00 -0500

    testing laus data model

--- extract/duckdb_test.py
+++ extract/duckdb_test.py
@@ -1,5 +1,10 @@
+#%%
 import duckdb
 con = duckdb.connect("dev.duckdb")
-con.execute("DROP TABLE raw_acs_series")
-print(con.execute("SHOW TABLES").df())
-print(con.execute("SELECT table_schema, table_name FROM information_schema.tables").df())
+# print(con.execute("SHOW TABLES").df())
+# print(con.execute("SELECT table_schema, table_name FROM information_schema.tables").df())
+# print(con.execute("SELECT * FROM raw_laus_series").df())
+# print(con.execute("SELECT * FROM raw_laus_data").df())
+# print(con.execute("SELECT * FROM raw_laus_data").df().describe())
+# print(con.execute("SELECT * FROM raw_laus_data").df().dtypes)
+print(con.execute("SELECT * FROM stg_laus_data ORDER BY data_date LIMIT 10").df())
