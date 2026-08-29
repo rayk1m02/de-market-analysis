e12a42f9e18e76262c486c417cd29f2eb6342743
Raymond Kim <hkapray@gmail.com>
2026-08-29 12:58:16 -0500

    implement laus data stg file. remove M13 (annual period)

---
+++ models/staging/stg_laus_data.sql
@@ -0,0 +1,16 @@
+WITH source AS (
+    SELECT * from {{ source('raw', 'raw_laus_data') }}
+),
+
+renamed AS (
+    SELECT 
+        series_id,
+        CAST(year || '-' || SUBSTRING(period, 2, 2) || '-01' AS DATE) AS data_date,
+        period,
+        period_name, 
+        CAST(value AS FLOAT) as value,
+    FROM source
+    WHERE period != 'M13'
+)
+
+select * from renamed
