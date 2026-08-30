c77d7e96346bdea43002c716cb8cde7ed3d8a931
Raymond Kim <hkapray@gmail.com>
2026-08-29 21:38:29 -0500

    viewing stg_oews_data

--- extract/duckdb_test.py
+++ extract/duckdb_test.py
@@ -1,15 +1,22 @@
 #%%
 import duckdb
-con = duckdb.connect("dev.duckdb")
+import os
+print(os.getcwd())
+# con = duckdb.connect("dev.duckdb")
+con = duckdb.connect(r"C:\Users\rkim\Desktop\Learning\de-market-analysis\dev.duckdb")
+
 # print(con.execute("SHOW TABLES").df())
-# print(con.execute("SELECT table_schema, table_name FROM information_schema.tables").df())
-print(con.execute("SELECT * FROM raw_oews_series").df())
-print("")
-print(con.execute("SELECT * FROM raw_oews_data").df())
-print("")
-print(con.execute("SELECT * FROM raw_oews_series").df().dtypes)
-print("")
-print(con.execute("SELECT * FROM raw_oews_data").df().dtypes)
-print("")
+#print(con.execute("SELECT table_schema, table_name FROM information_schema.tables").df())
+# print(con.execute("SELECT * FROM raw_oews_series").df())
+# print("")
+# print(con.execute("SELECT * FROM raw_oews_data").df())
+# print("")
+# print(con.execute("SELECT * FROM raw_oews_series").df().dtypes)
+# print("")
+# print(con.execute("SELECT * FROM raw_oews_data").df().dtypes)
+# print("")
 # print(con.execute("SELECT * FROM stg_laus_data ORDER BY data_date LIMIT 10").df())
 # print(con.execute("SELECT * FROM stg_laus_series").df())
+print(con.execute("SELECT * FROM stg_oews_data ORDER BY year LIMIT 20").df())
+# print(con.execute("SELECT * FROM raw_oews_data").df())
+# %%
