e7ea962654b6a77d38234b1a5d6c6fb8a669c3b3
Raymond Kim <hkapray@gmail.com>
2026-08-30 20:48:22 -0500

    8.48PM

--- extract/duckdb_test.py
+++ extract/duckdb_test.py
@@ -7,4 +7,8 @@ pd.set_option('display.max_colwidth', None)
 # con = duckdb.connect("dev.duckdb")
 con = duckdb.connect(r"C:\Users\rkim\Desktop\Learning\de-market-analysis\dev.duckdb")
 
+con.execute("SELECT * FROM stg_laus_series").df() 
+con.execute("SELECT * FROM stg_laus_data").df()
 con.execute("SELECT * FROM stg_oews_series").df()
+con.execute("SELECT * FROM stg_oews_data").df()
+con.execute("SELECT * FROM stg_acs_data").df()
