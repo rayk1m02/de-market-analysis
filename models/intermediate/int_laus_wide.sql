4d4d82f8958df4617e5608e4fab4b4e4367b4ce7
Raymond Kim <hkapray@gmail.com>
2026-08-30 21:08:39 -0500

    case statements for measure_type

--- models/intermediate/int_laus_wide.sql
+++ models/intermediate/int_laus_wide.sql
@@ -1,10 +1,26 @@
 SELECT
     s.area_abbr AS area_abbr,
     d.data_date AS data_date,
-    '' AS unemployment_rate,
-    '' AS unemployment,
-    '' AS employment,
-    '' AS labor_force
+    CASE
+        WHEN s.measure_type = 'unemployment_rate'
+            THEN d.value 
+        ELSE NULL
+    END AS unemployment_rate,
+    CASE
+        WHEN s.measure_type = 'unemployment'
+            THEN d.value 
+        ELSE NULL
+    END AS unemployment,
+    CASE
+        WHEN s.measure_type = 'employment'
+            THEN d.value 
+        ELSE NULL
+    END AS employment,
+    CASE
+        WHEN s.measure_type = 'labor_force'
+            THEN d.value 
+        ELSE NULL
+    END AS labor_force
 FROM {{ ref('stg_laus_series') }} s
 LEFT JOIN {{ ref('stg_laus_data') }} d
 ON d.series_id = s.series_id
