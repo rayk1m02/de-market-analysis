655e859b5f02c5a6e8f3dadf542c1c218681f387
Raymond Kim <hkapray@gmail.com>
2026-08-30 19:30:36 -0500

    viewing stg models, plan for int/fct/dim models

--- extract/duckdb_test.py
+++ extract/duckdb_test.py
@@ -1,23 +1,10 @@
 #%%
 import duckdb
 import os
-print(os.getcwd())
+import pandas as pd
+pd.set_option('display.max_columns', None)
+pd.set_option('display.max_colwidth', None)
 # con = duckdb.connect("dev.duckdb")
 con = duckdb.connect(r"C:\Users\rkim\Desktop\Learning\de-market-analysis\dev.duckdb")
 
-# print(con.execute("SHOW TABLES").df())
-#print(con.execute("SELECT table_schema, table_name FROM information_schema.tables").df())
-# print(con.execute("SELECT * FROM raw_oews_series").df())
-# print("")
-# print(con.execute("SELECT * FROM raw_acs_data").df())
-# print("")
-# print(con.execute("SELECT * FROM raw_acs_data").df().dtypes)
-# print("")
-# print(con.execute("SELECT * FROM raw_oews_data").df().dtypes)
-# print("")
-# print(con.execute("SELECT * FROM stg_laus_data ORDER BY data_date LIMIT 10").df())
-# print(con.execute("SELECT * FROM stg_laus_series").df())
-# print(con.execute("SELECT * FROM stg_oews_data ORDER BY year LIMIT 20").df())
-# print(con.execute("SELECT * FROM raw_oews_data").df())
-print(con.execute("SELECT * FROM stg_acs_data ORDER BY income").df())
-# %%
+con.execute("SELECT * FROM stg_oews_series").df()
