66f5cda4b745329937336a199252f43952a8d4cf
Raymond Kim <hkapray@gmail.com>
2026-08-31 20:26:50 -0500

    viewing oews series and data

--- extract/duckdb_test.py
+++ extract/duckdb_test.py
@@ -4,11 +4,15 @@ import os
 import pandas as pd
 pd.set_option('display.max_columns', None)
 pd.set_option('display.max_colwidth', None)
+pd.set_option('display.max_rows', None)
 # con = duckdb.connect("dev.duckdb")
 con = duckdb.connect(r"C:\Users\rkim\Desktop\Learning\de-market-analysis\dev.duckdb")
 
 con.execute("SELECT * FROM stg_laus_series").df() 
 con.execute("SELECT * FROM stg_laus_data").df()
-con.execute("SELECT * FROM stg_oews_series").df()
-con.execute("SELECT * FROM stg_oews_data").df()
+con.execute("SELECT * FROM stg_oews_series LIMIT 5").df()
+con.execute("SELECT * FROM stg_oews_data LIMIT 5").df()
 con.execute("SELECT * FROM stg_acs_data").df()
+
+con.execute("SELECT * FROM int_laus_wide ORDER BY area_abbr, data_date DESC").df()
+con.execute("SELECT DISTINCT series_title FROM stg_oews_series WHERE area_abbr = 'DC' and occupation_name = 'Database Administrators'").df()
