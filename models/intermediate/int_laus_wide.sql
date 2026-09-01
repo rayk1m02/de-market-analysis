bd8214db6837231423c3c18e6b6902843494b286
Raymond Kim <hkapray@gmail.com>
2026-08-31 19:39:38 -0500

    intermediate model for laus data

--- models/intermediate/int_laus_wide.sql
+++ models/intermediate/int_laus_wide.sql
@@ -1,26 +1,11 @@
 SELECT
     s.area_abbr AS area_abbr,
     d.data_date AS data_date,
-    CASE
-        WHEN s.measure_type = 'unemployment_rate'
-            THEN d.value 
-        ELSE NULL
-    END AS unemployment_rate,
-    CASE
-        WHEN s.measure_type = 'unemployment'
-            THEN d.value 
-        ELSE NULL
-    END AS unemployment,
-    CASE
-        WHEN s.measure_type = 'employment'
-            THEN d.value 
-        ELSE NULL
-    END AS employment,
-    CASE
-        WHEN s.measure_type = 'labor_force'
-            THEN d.value 
-        ELSE NULL
-    END AS labor_force
-FROM {{ ref('stg_laus_series') }} s
-LEFT JOIN {{ ref('stg_laus_data') }} d
-ON d.series_id = s.series_id
+    MAX(CASE WHEN s.measure_type = 'unemployment_rate' THEN d.value END) AS unemployment_rate,
+    MAX(CASE WHEN s.measure_type = 'unemployment' THEN d.value END) AS unemployment,    
+    MAX(CASE WHEN s.measure_type = 'employment' THEN d.value END) AS employment,
+    MAX(CASE WHEN s.measure_type = 'labor_force' THEN d.value END) AS labor_force
+FROM {{ ref('stg_laus_data') }} d
+LEFT JOIN {{ ref('stg_laus_series') }} s
+    ON d.series_id = s.series_id
+GROUP BY s.area_abbr, d.data_date
