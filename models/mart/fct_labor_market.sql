a9cfc3f63518f3ea59614e9c1047ec4805f82580
Raymond Kim <hkapray@gmail.com>
2026-09-02 17:11:29 -0500

    added unemp rate pp change, unemp rate 3mo avg, labor market rank per month

--- models/mart/fct_labor_market.sql
+++ models/mart/fct_labor_market.sql
@@ -9,7 +9,17 @@ SELECT
     ROUND((
         l.employment - (LAG(l.employment) OVER (PARTITION BY l.area_abbr ORDER by l.data_date))) / 
         (LAG(l.employment) OVER (PARTITION BY l.area_abbr ORDER BY l.data_date)
-    ) * 100, 2) AS emp_pct_change
+    ) * 100, 2) AS emp_pct_change,
+    ROUND((l.unemployment_rate - (LAG(l.unemployment_rate) OVER (PARTITION By l.area_abbr ORDER BY l.data_date))), 2) AS unemp_rate_pp_change,
+    ROUND(AVG(l.unemployment_rate) OVER (
+        PARTITION BY l.area_abbr
+        ORDER BY l.data_date
+        ROWS BETWEEN 2 PRECEDING AND CURRENT ROW
+    ), 2) AS unemp_rate_3mo_avg,
+    RANK() OVER (
+        PARTITION BY l.data_date
+        ORDER BY l.unemployment_rate ASC
+    ) AS labor_market_rank
 FROM {{ ref('int_laus_wide') }} l
 LEFT JOIN {{ ref('dim_metro') }} d
     ON l.area_abbr = d.area_abbr
