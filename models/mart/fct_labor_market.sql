120d4c71f3514971e9956c11d8f0e2576aa209da
Raymond Kim <hkapray@gmail.com>
2026-09-02 16:22:15 -0500

    added employment percent change column

--- models/mart/fct_labor_market.sql
+++ models/mart/fct_labor_market.sql
@@ -6,7 +6,10 @@ SELECT
     l.unemployment,
     l.employment,
     l.labor_force,
-    'Compute Metrics of Interest and Value'
+    ROUND((
+        l.employment - (LAG(l.employment) OVER (PARTITION BY l.area_abbr ORDER by l.data_date))) / 
+        (LAG(l.employment) OVER (PARTITION BY l.area_abbr ORDER BY l.data_date)
+    ) * 100, 2) AS emp_pct_change
 FROM {{ ref('int_laus_wide') }} l
 LEFT JOIN {{ ref('dim_metro') }} d
     ON l.area_abbr = d.area_abbr
