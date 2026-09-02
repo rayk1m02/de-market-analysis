4c247e749b5cea8fc566dc7e1c2fcbaa1e164df5
Raymond Kim <hkapray@gmail.com>
2026-09-02 18:25:54 -0500

    add base template for fct occupation

--- models/mart/fct_occupation.sql
+++ models/mart/fct_occupation.sql
@@ -1,1 +1,13 @@
--- from int_oews_wide
+SELECT
+    d.area_abbr,
+    d.area_name,
+    o.occupation_name,
+    o.year,
+    o.employment,
+    o.hourly_mean_wage,
+    o.annual_mean_wage,
+    o.annual_median_wage,
+    o.location_quotient
+FROM {{ ref('int_oews_wide') }} o
+LEFT JOIN {{ ref('dim_metro') }} d
+    ON o.area_abbr = d.area_abbr
