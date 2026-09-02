7909ddf52bdb9ae23983dfdc5dbb8388ef4be03b
Raymond Kim <hkapray@gmail.com>
2026-09-01 21:47:50 -0500

    base of fct laus model

--- models/mart/fct_labor_market.sql
+++ models/mart/fct_labor_market.sql
@@ -1,1 +1,12 @@
--- from int_laus_wide
+SELECT
+    d.area_abbr,
+    d.area_name,
+    l.data_date,
+    l.unemployment_rate,
+    l.unemployment,
+    l.employment,
+    l.labor_force,
+    'Compute Metrics of Interest and Value'
+FROM {{ ref('int_laus_wide') }} l
+LEFT JOIN {{ ref('dim_metro') }} d
+    ON l.area_abbr = d.area_abbr
