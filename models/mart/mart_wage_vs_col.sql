16a14005d9547a3ced61a5bc1df0dcf383d04bbb
Raymond Kim <hkapray@gmail.com>
2026-09-04 22:14:52 -0500

    analysis model

---
+++ models/mart/mart_wage_vs_col.sql
@@ -0,0 +1,11 @@
+/**
+core
+- DE-adjacent wages adjusted for cost of living, per metro
+    fct_occupation x fct_cost_of_living
+
+other
+- correlation between annual mean wage and location quotient (fct_occupation). 
+    does higher wage concentration track with higher ocupational density?
+- correlation between annual mean wage and employment, factoring in location quotient
+    does a larger occupational workforce relate to wage level, and does concentration change that relationship?
+*/
