b3b370f92fbc75d1dfb61cf95190da3cd36adf07
Raymond Kim <hkapray@gmail.com>
2026-09-05 11:25:01 -0500

    add documentation

--- models/mart/mart_wage_vs_col.sql
+++ models/mart/mart_wage_vs_col.sql
@@ -8,4 +8,7 @@ other
     does higher wage concentration track with higher ocupational density?
 - correlation between annual mean wage and employment, factoring in location quotient
     does a larger occupational workforce relate to wage level, and does concentration change that relationship?
+- employment percentage per occupation, per metro
+    fct_occupation.employment / fct_labor_market.labor_force
+    note - grain mismatch (monthly vs. annual snapshot) needs a decision on which labor_force month to use before building this
 */
