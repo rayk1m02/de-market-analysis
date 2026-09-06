de88ce3d6fbe726c400126c9b43dcb8a635b10c9
Raymond Kim <hkapray@gmail.com>
2026-09-05 20:44:40 -0500

    fct model for stg acs data

--- models/mart/fct_cost_of_living.sql
+++ models/mart/fct_cost_of_living.sql
@@ -1,1 +1,11 @@
--- from stg_acs_data
+SELECT
+    d.area_abbr,
+    d.area_name,
+    a.income,
+    a.rent,
+    a.home_value,
+    a.metro_code,
+    RANK() OVER (ORDER BY rent DESC) AS rent_rank_asc
+FROM {{ ref('stg_acs_data') }} a
+LEFT JOIN {{ ref('dim_metro') }} d
+    ON a.area_abbr = d.area_abbr
