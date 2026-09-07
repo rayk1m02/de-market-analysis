1b539d37f2e93e4db1a588ff7904b151cbbacca7
Raymond Kim <hkapray@gmail.com>
2026-09-06 21:13:58 -0500

    viewing all dbt data

--- extract/duckdb_test.py
+++ extract/duckdb_test.py
@@ -18,29 +18,78 @@ con.execute("SELECT * FROM int_laus_wide ORDER BY area_abbr, data_date DESC LIMI
 con.execute("SELECT * FROM int_oews_wide ORDER BY area_abbr, occupation_name LIMIT 25").df()
 
 con.execute("SELECT * FROM dim_metro").df()
-con.execute("SELECT * FROM fct_labor_market ORDER BY area_abbr DESC, data_date DESC LIMIT 10").df()
 
 con.execute("""
-    SELECT area_abbr, occupation_name, employment, annual_mean_wage, annual_median_wage, location_quotient, occupation_wage_rank, metro_wage_rank
+    SELECT 
+        area_abbr, 
+        income, 
+        rent, 
+        home_value, 
+        metro_code, 
+        rent_rank_asc 
+    FROM fct_cost_of_living
+    """).df()
+
+con.execute("""
+    SELECT 
+        area_abbr, 
+        data_date, 
+        unemployment_rate, 
+        unemployment, 
+        employment, 
+        labor_force, 
+        emp_pct_change, 
+        unemp_rate_pp_change, 
+        unemp_rate_3mo_avg, 
+        labor_market_rank  
+    FROM fct_labor_market 
+    ORDER BY area_abbr DESC, data_date DESC 
+    LIMIT 10
+    """).df()
+
+con.execute("""
+    SELECT 
+        area_abbr, 
+        occupation_name, 
+        employment, 
+        annual_mean_wage, 
+        annual_median_wage, 
+        location_quotient, 
+        occupation_wage_rank, 
+        metro_wage_rank
     FROM fct_occupation
     ORDER BY area_abbr, occupation_wage_rank
     """).df()
-con.execute("SELECT area_abbr, income, rent, home_value, metro_code, rent_rank_asc FROM fct_cost_of_living").df()
-
-con.execute("""
-    SELECT area_abbr, occupation_name, annual_mean_wage, rent, home_value, rent_pct_of_wage, years_to_afford_home 
-    FROM mart_wage_vs_col 
-    ORDER BY area_abbr, rent_pct_of_wage ASC
-    """).df()
-
 con.execute("SELECT CORR(annual_mean_wage, location_quotient) FROM fct_occupation").df()
 con.execute("SELECT CORR(annual_mean_wage, employment) FROM fct_occupation").df()
 
-con.execute("SELECT area_abbr, occupation_name, employment, avg_labor_force_2025, occupation_share_pct FROM mart_occupation_share").df()
 con.execute("SELECT * FROM mart_labor_force_vs_col").df()
 con.execute("""
     SELECT 
         CORR(avg_labor_force_2025, rent) AS corr_labor_force_rent,
         CORR(avg_labor_force_2025, home_value) AS corr_labor_force_home_value
     FROM mart_labor_force_vs_col
-""").df()
+    """).df()
+
+con.execute("""
+    SELECT 
+        area_abbr, 
+        occupation_name, 
+        employment, 
+        avg_labor_force_2025, 
+        occupation_share_pct 
+    FROM mart_occupation_share
+    """).df()
+
+con.execute("""
+    SELECT 
+        area_abbr, 
+        occupation_name, 
+        annual_mean_wage, 
+        rent, 
+        home_value, 
+        rent_pct_of_wage, 
+        years_to_afford_home 
+    FROM mart_wage_vs_col 
+    ORDER BY area_abbr, rent_pct_of_wage ASC
+    """).df()
